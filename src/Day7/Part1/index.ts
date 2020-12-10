3fcdee60bf8838ea9ff01148ab02a133643e0db8
spenceradolph <spenceradolph@tamu.edu>
2020-12-09 21:17:53 -0600

    cleaned some code, added comments

--- src/Day7/Part1/index.ts
+++ src/Day7/Part1/index.ts
@@ -2,8 +2,10 @@ import { input } from '../input';
 
 const canItHold = (bagColor: string, searchColor: string) => {
     for (let x = 0; x < input.length; x++) {
+        // Find bagColor in the list
         if (input[x].color == bagColor) {
             for (const insideBag of input[x].inside) {
+                // Consider each child
                 if (insideBag.color == searchColor) return true;
                 if (canItHold(insideBag.color, searchColor)) return true; // don't return on false...keep searching
             }
@@ -19,7 +21,7 @@ export const Day7Part1 = () => {
     let numContainShinyGold = 0;
 
     for (const bag of input) {
-        numContainShinyGold += canItHold(bag.color, 'shiny gold') ? 1 : 0;
+        numContainShinyGold += canItHold(bag.color, 'shiny gold') ? 1 : 0; // Each top-level parent eith can or can't
     }
 
     console.log(numContainShinyGold);
--- src/Day7/Part2/index.ts
+++ src/Day7/Part2/index.ts
@@ -4,17 +4,18 @@ const numBags = (searchColor: string): number => {
     let numInsideThisBag = 0;
 
     for (const bag of input) {
+        // Find searchColor in master list
         if (bag.color == searchColor) {
             for (const insideBag of bag.inside) {
                 numInsideThisBag += insideBag.number;
                 numInsideThisBag += numBags(insideBag.color) * insideBag.number;
             }
 
-            break;
+            return numInsideThisBag;
         }
     }
 
-    return numInsideThisBag;
+    return -1; // could not find the searchColor
 };
 
 export const Day7Part2 = () => {
