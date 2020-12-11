c9bea83f37ef9a418ac8d5c51a4243ecf4d9f63d
spenceradolph <spenceradolph@tamu.edu>
2020-12-11 11:47:04 -0600

    refactor Day3Part2

--- src/Day3/Part2/index.ts
+++ src/Day3/Part2/index.ts
@@ -1,57 +1,23 @@
 import { input, TREE } from '../input';
 
-export const Day3Part2 = () => {
+const helper = (slopeRight: number, slopeDown: number) => {
     const lengthOfLine = input[0].length;
-
-    // Right 1, Down 1 // TODO: make this a function call to simplify
-    let numTreeHits1 = 0;
-    let slopeRight = 1;
-    let slopeDown = 1;
+    let numTreeHits = 0;
     let currentX = 0;
     for (let lineNumber = slopeDown; lineNumber < input.length; lineNumber += slopeDown) {
         currentX = (currentX + slopeRight) % lengthOfLine;
-        if (input[lineNumber][currentX] == TREE) numTreeHits1++;
+        if (input[lineNumber][currentX] == TREE) numTreeHits++;
     }
 
-    // Right 3, Down 1
-    let numTreeHits2 = 0;
-    slopeRight = 3;
-    slopeDown = 1;
-    currentX = 0;
-    for (let lineNumber = slopeDown; lineNumber < input.length; lineNumber += slopeDown) {
-        currentX = (currentX + slopeRight) % lengthOfLine;
-        if (input[lineNumber][currentX] == TREE) numTreeHits2++;
-    }
+    return numTreeHits;
+};
 
-    // Right 5, Down 1
-    let numTreeHits3 = 0;
-    slopeRight = 5;
-    slopeDown = 1;
-    currentX = 0;
-    for (let lineNumber = slopeDown; lineNumber < input.length; lineNumber += slopeDown) {
-        currentX = (currentX + slopeRight) % lengthOfLine;
-        if (input[lineNumber][currentX] == TREE) numTreeHits3++;
-    }
-
-    // Right 7, Down 1
-    let numTreeHits4 = 0;
-    slopeRight = 7;
-    slopeDown = 1;
-    currentX = 0;
-    for (let lineNumber = slopeDown; lineNumber < input.length; lineNumber += slopeDown) {
-        currentX = (currentX + slopeRight) % lengthOfLine;
-        if (input[lineNumber][currentX] == TREE) numTreeHits4++;
-    }
-
-    // Right 1, Down 2
-    let numTreeHits5 = 0;
-    slopeRight = 1;
-    slopeDown = 2;
-    currentX = 0;
-    for (let lineNumber = slopeDown; lineNumber < input.length; lineNumber += slopeDown) {
-        currentX = (currentX + slopeRight) % lengthOfLine;
-        if (input[lineNumber][currentX] == TREE) numTreeHits5++;
-    }
+export const Day3Part2 = () => {
+    let numTreeHits1 = helper(1, 1);
+    let numTreeHits2 = helper(3, 1);
+    let numTreeHits3 = helper(5, 1);
+    let numTreeHits4 = helper(7, 1);
+    let numTreeHits5 = helper(1, 2);
 
     console.log(numTreeHits1 * numTreeHits2 * numTreeHits3 * numTreeHits4 * numTreeHits5);
 };
